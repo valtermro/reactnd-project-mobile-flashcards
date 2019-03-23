c7b7de6857ee0b351fc0c05f42b1e8777c4ee33d
valtermro <valtermro@outlook.com>
2019-03-23 13:23:44 -0300

    Add empty state view to the DeckList view.

--- app/views/DeckList.js
+++ app/views/DeckList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import * as colors from '../colors';
 import { getAppState } from '../store';
 import { getAllDecks } from '../decks/store';
 import { getCardsByDeck } from '../cards/store';
 import DeckListEntry from '../decks/DeckListEntry';
+import ActionButton from '../components/ActionButton';
 
 function mapStateToProps(state) {
   return {
@@ -15,6 +17,21 @@ function mapStateToProps(state) {
     })),
   };
 }
+
+const styles = StyleSheet.create({
+  emptyContainer: {
+    alignItems: 'center',
+  },
+  emptyHeader: {
+    color: colors.textDefault,
+    marginTop: 80,
+    fontSize: 16,
+  },
+  createDeckButton: {
+    backgroundColor: colors.primary,
+    marginTop: 40,
+  },
+});
 
 class DeckList extends React.Component {
   keyExtractor = (deck) => {
@@ -35,13 +52,30 @@ class DeckList extends React.Component {
   }
 
   render = () => {
-    const { loading, decks } = this.props;
+    const { loading, decks, navigation } = this.props;
 
     if (loading) {
       return (
         <View>
           {/* TODO */}
           <Text>Loading...</Text>
+        </View>
+      );
+    }
+
+    if (decks.length === 0) {
+      return (
+        <View style={styles.emptyContainer}>
+          <Text style={styles.emptyHeader}>
+            You don't have any decks yet.
+          </Text>
+
+          <ActionButton
+            style={styles.createDeckButton}
+            onPress={() => navigation.navigate('NewDeck')}
+          >
+            Create a deck
+          </ActionButton>
         </View>
       );
     }
